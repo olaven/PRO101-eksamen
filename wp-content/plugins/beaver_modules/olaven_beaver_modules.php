ffebb0e5ea064b21ac2e25d6aca9e97c0535c07a
olaven <olavsundfoer@gmail.com>
2018-05-02 23:28:10 +0200

    la til ruter modul #63

--- wp-content/plugins/beaver_modules/olaven_beaver_modules.php
+++ wp-content/plugins/beaver_modules/olaven_beaver_modules.php
@@ -5,13 +5,14 @@
  * Version: 1.0
  * Author: Olav
  */
-define( 'CAMPUS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'CAMPUS_PLUGIN_URL', plugins_url( '/', __FILE__ ) );
+define( 'BEAVER_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'BEAVER_PLUGIN_URL', plugins_url( '/', __FILE__ ) );
 
 function my_load_module_examples() {
     if ( class_exists( 'FLBuilder' ) ) {
         // Include your custom modules here.
         require_once 'campus/campus.php';
+        require_once 'ruterwidget/ruterwidget.php';
     }
 }
 add_action( 'init', 'my_load_module_examples' );
---
+++ wp-content/plugins/beaver_modules/ruterwidget/includes/frontend.php
@@ -0,0 +1,8 @@
+<iframe 
+    src='<?php echo "https://ruter.no/iframe-search.html#Til%20( " . $settings->id . ")" . $settings->navn . "$?orientation=horizontal&logo=nologo "?>' 
+    allowTransparency='true' 
+    frameborder='0' 
+    scrolling='no' 
+    height='354' 
+    width='420'>
+</iframe>
