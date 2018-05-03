46b046ee1e0e6e4044aff32e5a0c511a4759129a
olaven <olavsundfoer@gmail.com>
2018-05-03 09:37:48 +0200

    ruter-modul fungerer #63

--- wp-content/plugins/beaver_modules/ruterwidget/includes/frontend.php
+++ wp-content/plugins/beaver_modules/ruterwidget/includes/frontend.php
@@ -6,4 +6,3 @@
     height='354' 
     width='420'>
 </iframe>
-<iframe src='https://ruter.no/iframe-search.html#Til%20(30111077)Christian%20Krohgs%20gate%2032B%20(Oslo)%7CFjerdingen$' allowTransparency='true' frameborder='0' scrolling='no' height='354' width='180'></iframe>
