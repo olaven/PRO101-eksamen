4d86be6bc55acbaac819aa7ab2456b27b85e18a0
olaven <olavsundfoer@gmail.com>
2018-05-18 21:45:59 +0200

    startet arbeid med å tilpasse designmanual #65

--- wp-content/plugins/beaver_modules/campus/includes/frontend.php
+++ wp-content/plugins/beaver_modules/campus/includes/frontend.php
@@ -1,22 +1,47 @@
-
+<style>
+    .campus img{
+        float: left; 
+        width: 60%; 
+    }
+    .campus article{
+        float: right; 
+        width: 40%; 
+    }
+    .campus-navigation{
+        display: flex; 
+        width: 100%; 
+    }
+    .campus-navigation > div{
+        width: 100%; 
+        border-style: solid;
+    }
+    .campus-navigation > div:target{
+        transform: scale(1.1); 
+    }
+</style>
 <div class="campus">
     <img src="<?php echo $settings->bilde_src; ?>" />
-    <?php echo $settings->navn; ?>
-    <br/>
-    <?php 
-        date_default_timezone_set("Europe/Oslo"); 
-        $current_hour = date("G");
-        $opening_hour = ($settings->apent_fra['day_period'] == "am" ? $settings->apent_fra["hours"] : ($settings->apent_fra["hours"] + 12)); 
-        $closing_hour = ($settings->apent_til['day_period'] == "am" ? $settings->apent_til["hours"] : ($settings->apent_til["hours"] + 12)); 
+    <article>
+        <h2>
+            <?php echo $settings->navn; ?>
+        </h2>
+        <nav class="campus-navigation">
+            <div>Kontakt</div>
+            <div>Åpningsider</div>
+            <div>Kantine</div>
+        </nav>
+        <?php 
+            date_default_timezone_set("Europe/Oslo"); 
+            $current_hour = date("G");
+            $opening_hour = ($settings->apent_fra['day_period'] == "am" ? $settings->apent_fra["hours"] : ($settings->apent_fra["hours"] + 12)); 
+            $closing_hour = ($settings->apent_til['day_period'] == "am" ? $settings->apent_til["hours"] : ($settings->apent_til["hours"] + 12)); 
 
-        if($current_hour > $opening_hour && $current_hour < $closing_hour){
-            echo "Åpent: JA"; 
-        } else {
-            echo "Åpent: NEI"; 
-        }
-    ?>
-    <br/>
-    <button onClick="alert('ikke implementert -@olaven')">Vis veien</button> 
-    <hr>
+            if($current_hour > $opening_hour && $current_hour < $closing_hour){
+                echo "Åpent: JA"; 
+            } else {
+                echo "Åpent: NEI"; 
+            }
+        ?>
+    </article>
 </div>
 
