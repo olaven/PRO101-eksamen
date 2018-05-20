9df478a24932ebba42544090e55fc2f776a71d85
Safet Zahirovic <zahirovic.safet@gmail.com>
2018-05-20 22:09:53 +0200

    Lagt til mulighet til å velge størrelsen i beaver.

--- wp-content/plugins/beaver_modules/ruterwidget/includes/frontend.php
+++ wp-content/plugins/beaver_modules/ruterwidget/includes/frontend.php
@@ -1,8 +1,11 @@
 <iframe 
-    src='<?php echo "https://ruter.no/iframe-search.html#Til%20(" . (string) $settings->ruter_id . ")" . $settings->gatenavn . "%7C" . $settings->navn . "$?logo=nologo "?>' 
+    src='<?php echo "https://ruter.no/iframe-search.html#Til%20(" . (string) $settings->ruter_id . ")" . $settings->gatenavn . "%7C" . $settings->navn . "$?orientation=horizontal&logo=nologo "?>' 
+    style=""
     allowTransparency='true' 
     frameborder='0' 
-    scrolling='no' 
-    height='354' 
-    width='420'>
+    scrolling='no'
+    height="<?php echo $settings->height; ?>" 
+    width="<?php echo $settings->width; ?>">
 </iframe>
+    
+
