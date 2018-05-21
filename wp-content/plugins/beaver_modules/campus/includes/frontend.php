03c1f6e6b48b0d2c28707e325558434e1a62afed
olaven <olavsundfoer@gmail.com>
2018-05-21 15:05:00 +0200

    tips fungerer med wordpress posts :D #78

--- wp-content/plugins/beaver_modules/campus/includes/frontend.php
+++ wp-content/plugins/beaver_modules/campus/includes/frontend.php
@@ -112,6 +112,30 @@
             </article>
             <article id="campus-tips" class="articles">
                 <h2>TIPS FRA OSS!</h2>
+                <?php                 echo strtolower($settings->navn); ?> 
+                <?php 
+                // the query
+                $wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'tag'=>strtolower($settings->navn), 'posts_per_page'=>-1)); ?>
+                
+                <?php if ( $wpb_all_query->have_posts() ) : ?>
+                
+                <ul>
+                
+                    <!-- the loop -->
+                    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+                        <h4><?php the_title(); ?></h4>
+                        <p><?php the_content(); ?></p>
+
+                    <?php endwhile; ?>
+                    <!-- end of the loop -->
+                
+                </ul>
+                
+                    <?php wp_reset_postdata(); ?>
+                
+                <?php else : ?>
+                    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                <?php endif; ?>
             </article>
         </section>
     </article>
